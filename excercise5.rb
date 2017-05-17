1e54c0b8ac10df318865679ef4e1ffc46979fdd4
Don Cooper <don_cooper@hotmail.com>
2017-05-17 17:15:11 -0400

    Starting excercise 10

--- excercise5.rb
+++ excercise5.rb
@@ -57,3 +57,29 @@
 #------------
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def starred(list)
+  list.map do |item|
+    "* #{item}"
+  end
+end
+
+grocery_list << "rice"
+
+starred(grocery_list)
+
+# puts starred(grocery_list).length
+
+# starred(grocery_list).include?("bananas")
+# if true
+#   puts "You need to pick up bananas."
+# else
+#   puts "You don't need to pick up bananas today."
+# end
+
+# puts starred(grocery_list)[1]
+
+# puts starred(grocery_list).sort
+
+# grocery_list.delete("salmon")
+# puts starred(grocery_list)
