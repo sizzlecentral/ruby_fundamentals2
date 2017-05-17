c9af5fabc8fb0f02b407d3a75faabfd6d81a3c88
Don Cooper <don_cooper@hotmail.com>
2017-05-17 13:42:12 -0400

    Excercise 6

--- excercise5.rb
+++ excercise5.rb
@@ -1,11 +1,25 @@
 # Excercise 5
 #------------
 
-def greet_backwards(name)
-  puts "Hello #{name.reverse}#{name.reverse}!  Welcome home."
+# def greet_backwards(name)
+#   puts "Hello #{name.reverse}#{name.reverse}!  Welcome home."
+# end
+#
+# puts greet_backwards("Bob")
+# puts greet_backwards("Shirly")
+# puts greet_backwards("Sue")
+# puts greet_backwards("Andy")
+
+
+# Excercise 6
+#------------
+
+puts "Please enter a temperature in Fahrenheit."
+fahrenheit = gets.chomp.to_i
+
+def celcius_to_fahrenheit(temperature)
+  celcius = (temperature - 32) * 5/9
+  puts "The temperature in Celcius is #{celcius}."
 end
 
-puts greet_backwards("Bob")
-puts greet_backwards("Shirly")
-puts greet_backwards("Sue")
-puts greet_backwards("Andy")
+celcius_to_fahrenheit(fahrenheit)
