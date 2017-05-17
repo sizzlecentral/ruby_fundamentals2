d0f11f2ad9622140bfd72b31490dd0bec081e83c
Don Cooper <don_cooper@hotmail.com>
2017-05-17 13:30:11 -0400

    Excercise 5

---
+++ excercise5.rb
@@ -0,0 +1,11 @@
+# Excercise 5
+#------------
+
+def greet_backwards(name)
+  puts "Hello #{name.reverse}#{name.reverse}!  Welcome home."
+end
+
+puts greet_backwards("Bob")
+puts greet_backwards("Shirly")
+puts greet_backwards("Sue")
+puts greet_backwards("Andy")
