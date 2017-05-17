83211569dabe0bffd45a1a364f54374051d72169
Don Cooper <don_cooper@hotmail.com>
2017-05-17 16:21:28 -0400

    Starting excercise 9

--- excercise5.rb
+++ excercise5.rb
@@ -14,12 +14,46 @@
 # Excercise 6
 #------------
 
-puts "Please enter a temperature in Fahrenheit."
-fahrenheit = gets.chomp.to_i
+# puts "Please enter a temperature in Fahrenheit."
+# fahrenheit = gets.chomp.to_i
+#
+# def celcius_to_fahrenheit(temperature)
+#   celcius = (temperature - 32) * 5/9
+#   puts "The temperature in Celcius is #{celcius}."
+# end
+#
+# celcius_to_fahrenheit(fahrenheit)
 
-def celcius_to_fahrenheit(temperature)
-  celcius = (temperature - 32) * 5/9
-  puts "The temperature in Celcius is #{celcius}."
-end
 
-celcius_to_fahrenheit(fahrenheit)
+# Excercise 7
+#------------
+
+# def wrap_text(text, symbol)
+#   "#{symbol}#{text}#{symbol}"
+# end
+#
+# message = wrap_text("new message", "###")
+# message2 = wrap_text(message, "===")
+# message3 = wrap_text(message2, "---")
+#
+# puts message3
+
+
+# Excercise 8
+#------------
+
+# expenses = [250, 7.95, 30.95, 16.5]
+# expenses2 = [1, 2, 3, 4, 5]
+#
+# def arr_add(array)
+#   array.reduce(:+)
+# end
+#
+# puts arr_add(expenses)
+# puts arr_add(expenses2)
+
+
+# Excercise 9
+#------------
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
